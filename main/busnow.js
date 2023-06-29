32d4c12821bf741368453e0531446aba05944ddb
iE1000 <132737391+iE1000@users.noreply.github.com>
2023-06-29 17:03:06 -0500

    Update busnow.js

--- main/busnow.js
+++ main/busnow.js
@@ -1,133 +1,143 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiaWUxMDAwIiwiYSI6ImNsaXM4N2JldTFhbXAzZXBndHU4eTYxY2IifQ.1NGPST-1FECLZR8hOxS_yA';
-
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/ie1000/clis8cjc1002u01p78nsj4t43',
-  center: [-82.3980874155059, 8.429475976461935], // Coordenadas del centro del mapa
-  zoom: 17 // Nivel de zoom predeterminado
-});
-
-// Agregar controles al mapa
-map.addControl(new mapboxgl.NavigationControl());
-
-// Obtener una ruta optimizada entre múltiples ubicaciones
-var coordinates = [
-  [-82.42278762219792, 8.430611563578015], // Punto de inicio
-  [-82.39795060294597, 8.42895718362969], // Parada 1
-  [-82.38391843980015, 8.406776929721016],
-  [-82.38176318885175, 8.405364475685515],
-  [-82.38165973268401, 8.407387893036764],
-  [-82.38150735617234, 8.407653052115238],
-  // Agregar las coordenadas de las paradas adicionales aquí...
-];
-
+// Coordenadas de las paradas de buses
 var busStops = [
-  [-82.39795060294597, 8.42895718362969], // Parada 1
-  // Agregar las coordenadas de las paradas adicionales aquí...
+  [-82.42267611688577, 8.430658401132375], // Piquera de Las Lomas
+  [-82.4200617379403, 8.433666214773126], // Parada Hospital Cattan
+  [-82.41327687796455, 8.435678511511835], // Parada El Sapo
+  [-82.4051220253299, 8.430654238476235], // Parada Policía
+  [-82.39791399738645, 8.428969380717133], // Parada Chino San Jose
 ];
 
-var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + coordinates.join(';') + '?geometries=geojson&access_token=' + mapboxgl.accessToken;
+// Variable para almacenar el índice de la parada actual del bus
+var currentStopIndex = 0;
 
-// Realizar la solicitud a la API de direcciones de Mapbox
-fetch(directionsRequest)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    var route = data.routes[0].geometry;
+// Intervalo de tiempo en milisegundos para actualizar la ubicación del bus
+var updateInterval = 1000; // 1 segundo
 
-    // Agregar la ruta al mapa
-    map.on('load', function() {
-      // Agregar un marcador para la parada inicial
-      var startMarker = new mapboxgl.Marker({ color: 'blue' })
-        .setLngLat(busStops[0]) // Punto de inicio
-        .addTo(map);
+// Velocidad del bus en km/h
+var velocidadBus = 20;
 
-      // Agregar marcadores para las paradas de autobús
-      busStops.forEach(function(stop) {
-        var marker = new mapboxgl.Marker()
-          .setLngLat(stop)
-          .addTo(map);
-      });
+// Variable para almacenar el tiempo restante
+var tiempoRestante;
 
-      map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-          type: 'geojson',
-          data: {
-            type: 'Feature',
-            geometry: route
-          }
-        },
-        paint: {
-          'line-color': '#000', // Visualiza la ruta trazada
-          'line-width': 3
-        }
-      });
+// Función para calcular el tiempo restante hasta la próxima parada
+async function calcularTiempoRestante() {
+  var distancia = await obtenerDistanciaEntreParadas();
+  if (distancia !== null) {
+    var velocidadMetrosSegundo = velocidadBus * 1000 / 3600; // Convertir la velocidad a metros por segundo
+    var tiempo = distancia / velocidadMetrosSegundo;
+    return tiempo;
+  } else {
+    console.error('Error al obtener la distancia entre paradas.');
+    return null;
+  }
+}
 
-      // Crear un marcador para el ícono del bus
-      var busMarker = new mapboxgl.Marker({
-        color: 'red',
-        element: createBusIcon() // Función para crear el ícono del bus
-      }).setLngLat(busStops[0]) // Punto de inicio
-        .addTo(map);
+// Función para obtener la distancia en kilómetros entre la parada actual y la siguiente utilizando la API de direcciones de Mapbox
+async function obtenerDistanciaEntreParadas() {
+  var currentStop = busStops[currentStopIndex];
+  var nextStopIndex = (currentStopIndex + 1) % busStops.length;
+  var nextStop = busStops[nextStopIndex];
 
-      // Simular el desplazamiento del ícono del bus
-      var index = 0;
-      var interval = setInterval(function() {
-        index++;
-        if (index >= busStops.length) {
-          clearInterval(interval);
-          return;
-        }
+  var url = `https://api.mapbox.com/directions/v5/mapbox/driving/${currentStop[0]},${currentStop[1]};${nextStop[0]},${nextStop[1]}`;
+  var params = {
+    access_token: 'pk.eyJ1IjoiaWUxMDAwIiwiYSI6ImNsaXM4N2JldTFhbXAzZXBndHU4eTYxY2IifQ.1NGPST-1FECLZR8hOxS_yA',
+  };
 
-        busMarker.setLngLat(busStops[index]);
-      }, 2000); // Intervalo de tiempo entre paradas (2 segundos en este ejemplo)
-    });
+  try {
+    var response = await fetch(url + '?' + new URLSearchParams(params));
+    var data = await response.json();
+    var distancia = data.routes[0].distance / 1000; // La distancia se obtiene en metros, se divide por 1000 para obtener kilómetros
+    var route = data.routes[0].geometry.coordinates; // Coordenadas de la ruta
+    trazarRutaEnMapa(route); // Trazar la ruta en el mapa
+    return distancia;
+  } catch (error) {
+    console.error('Error al obtener la distancia:', error);
+    return null;
+  }
+}
+
+// Función para trazar la ruta en el mapa
+function trazarRutaEnMapa(route) {
+  map.getSource('route').setData({
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: route,
+    },
+  });
+}
+
+// Función para cambiar la paleta de colores
+function cambiarPaletaDeColores() {
+  var slider = document.getElementById('color-slider');
+  var color = slider.value;
+  document.documentElement.style.setProperty('--main-color', 'hsl(' + color + ', 100%, 50%)');
+}
+
+// Función para inicializar el mapa y la ruta
+function initMap() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiaWUxMDAwIiwiYSI6ImNsaXM4N2JldTFhbXAzZXBndHU4eTYxY2IifQ.1NGPST-1FECLZR8hOxS_yA';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: busStops[0],
+    zoom: 15,
   });
 
-// Función para crear el ícono del bus
-function createBusIcon() {
-  var icon = document.createElement('div');
-  icon.className = 'bus-icon';
-  return icon;
-}
-
-// Función para mostrar el formulario de ubicación del bus
-function mostrarFormularioUbicacion() {
-  // Aquí puedes implementar el código para mostrar el formulario de ubicación del bus
-  // Puede ser un cuadro emergente o una sección en la página
-  // Puedes utilizar JavaScript o una librería como Bootstrap para mostrar el formulario
-}
-
-// Función para calcular la distancia y el tiempo estimado de llegada
-function calcularDistanciaTiempo(latBus, lonBus, latParada, lonParada) {
-  // Realizar la solicitud a la API de Mapbox para obtener la información de distancia y tiempo
-  // Utiliza las coordenadas del bus (latBus, lonBus) y las coordenadas de la parada (latParada, lonParada)
-  // Puedes utilizar la librería 'axios' para realizar la solicitud HTTP
-  // Aquí tienes un ejemplo utilizando fetch:
-  var apiUrl = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + lonBus + ',' + latBus + ';' + lonParada + ',' + latParada;
-  apiUrl += '?access_token=' + mapboxgl.accessToken + '&alternatives=false&steps=true&geometries=geojson&overview=full';
-
-  fetch(apiUrl)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var route = data.routes[0];
-      var distance = route.distance; // Distancia en metros
-      var duration = route.duration; // Duración en segundos
-
-      // Convertir la distancia y la duración a minutos
-      var distanceMinutes = Math.round(distance / 60);
-      var durationMinutes = Math.round(duration / 60);
-
-      // Mostrar los resultados en la página
-      document.getElementById('distancia').textContent = distanceMinutes + ' minutos';
-      document.getElementById('tiempo').textContent = durationMinutes + ' minutos';
-    })
-    .catch(function(error) {
-      console.log(error);
+  map.on('load', function () {
+    // Agregar la capa de la ruta
+    map.addSource('route', {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: [],
+        },
+      },
     });
+
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: 'route',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round',
+      },
+      paint: {
+        'line-color': '#888',
+        'line-width': 8,
+      },
+    });
+
+    // Agrega un marcador en cada parada de buses
+    busStops.forEach(function (stop) {
+      var el = document.createElement('div');
+      el.className = 'bus-stop-marker';
+      new mapboxgl.Marker(el).setLngLat(stop).addTo(map);
+    });
+
+    // Crea el ícono del bus
+    var busIcon = document.createElement('div');
+    busIcon.className = 'bus-icon';
+
+    // Agrega el marcador del bus en la primera parada
+    var busMarker = new mapboxgl.Marker(busIcon).setLngLat(busStops[0]).addTo(map);
+
+    // Actualiza la ubicación del bus y el tiempo restante en intervalos regulares
+    setInterval(async function () {
+      var tiempoRestante = await calcularTiempoRestante();
+      if (tiempoRestante !== null) {
+        document.getElementById('tiempo-restante').textContent = 'Tiempo restante: ' + tiempoRestante.toFixed(1) + ' segundos';
+        busMarker.setLngLat(busStops[currentStopIndex]);
+        currentStopIndex = (currentStopIndex + 1) % busStops.length;
+      }
+    }, updateInterval);
+  });
 }
+
+// Inicializa el mapa cuando se carga la página
+document.addEventListener('DOMContentLoaded', initMap);
